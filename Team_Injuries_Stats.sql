8543fa081c21e1b3f992fd37ee09916caac7594a
CSwilliams88 <69878139+CSwilliams88@users.noreply.github.com>
2020-12-05 12:55:39 -0500

    Another Final Push

--- Team_Injuries_Stats.sql
+++ Team_Injuries_Stats.sql
@@ -1,3 +1,22 @@
+BULK INSERT injuries_impact
+    FROM 'Output/Injury_Count.csv'
+    WITH
+    (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+    )
+BULK INSERT injuries_impact
+    FROM 'Output/NBA_Team_Stats.csv'
+    WITH
+    (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',   
+    TABLOCK
+    )
+
 drop table injuries_impact
 
 create table injuries_impact (
